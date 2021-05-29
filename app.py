34eedce5f16628a0e0972b90973a1edce0c019e6
Zheng, Zhejun <jack.z-517@outlook.com>
2021-05-29 17:47:55 +0800

    进一步完善
    1. 增加支持模糊检索，可输入正则表达式
    2. 人物检索部分逻辑改为先筛选输入的检出结果，点击节点再显示人物关系。
    3. 优化若干处布局呈现。
    4. 增加事件检索，待完善。

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,request
-from py2neo import Graph,Node,Relationship,NodeMatcher,RelationshipMatcher,walk
+from py2neo import Graph,Node,Relationship,NodeMatcher,RelationshipMatcher,walk,LIKE
 from config_params import params
 
 app = Flask(__name__)
@@ -14,6 +14,7 @@ def hello_world():
 def query():
     if(request.form['field']=="人物"):
         query = request.form['query']
+        print(query)
         matcher = NodeMatcher(graph)
         node = matcher.match("Person_entity", name=query).first()
         if node:
@@ -28,19 +29,69 @@ def query():
             nodes = list(set(nodes))
             node_set = []
             for n in nodes:
-                node_set.append({"name":n})
+                node_set.append({"name":n,"nodeType":"Person_entity"})
             print(request.form)
             print("查询成功")
-            return {"data": node_set,"links": edges}
+            return {"data": node_set,"links": edges, "info":"当前检索策略下，检出 %d 个节点以及 %d 条连边。" % (len(node_set),len(edges))}
+        else:
+            print(request.form)
+            print("查询失败！")
+            return {"data": None, "info": "发生错误！请检查您的点击操作是否准确。"}
+    elif request.form['field']=="事件":
+        query = request.form['query']
+        # 在这里可以加一个模糊匹配查询
+        matcher = NodeMatcher(graph)
+        node = matcher.match("Object", name=query).first()
+        if node:
+            matcher = RelationshipMatcher(graph)
+            relations = matcher.match({node}).limit(10)
+            nodes = [node["name"]]
+            edges = []
+            for relation in relations:
+                nodes.append(relation.start_node["name"])
+                nodes.append(relation.end_node["name"])
+                edges.append({"source": relation.start_node["name"], "target": relation.end_node['name'],
+                              "value": relation["type"]})
+            nodes = list(set(nodes))
+            node_set = []
+            for n in nodes:
+                node_set.append({"name": n})
+            print(request.form)
+            print("查询成功")
+            return {"data": node_set, "links": edges}
         else:
             print(request.form)
             print("查询失败！")
             return ""
+    elif request.form['field']=="关系":
+        print(request.form)
+        print("查询失败！")
+        return ""
     else:
         print(request.form)
         print("查询失败！")
         return ""
 
+@app.route('/querynode',methods=['POST'])
+def query_node():
+    if request.form['field'] == '人物':
+        query = request.form['query']
+        matcher = NodeMatcher(graph)
+        nodes = matcher.match("Person_entity", name=LIKE(query)).all()
+        if nodes:
+            node_set = []
+            for node in nodes:
+                node_set.append({"name": node["name"],"nodeType":"Person_entity"})
+            return {"data": node_set, "links": {}, "info":"根据检索条件，共检出 %d 个匹配节点." % len(node_set)}
+        else:
+            print(request.form)
+            print("查询失败！")
+            return {"data": None, "info": "根据检索条件，无对应节点被检出。"}
+
+
+
+
+
+
 if __name__ == '__main__':
     app.run()
-
